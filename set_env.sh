b736107547a4b37afd0dd445b58c63b720ce5d86
davidbockelman <davidbockelman@utexas.edu>
2025-05-01 16:47:14 -0500

    added tPatchGNN to set_env script

--- set_env.sh
+++ set_env.sh
@@ -39,15 +39,28 @@ cd VQGraph
 pip install -r requirements.txt
 cd ..
 
-# 9. Install CUDA libraries (optional but safe)
+# 9. Clone and setup tPatchGNN
+git clone https://github.com/usail-hkust/t-PatchGNN.git
+cd t-PatchGNN
+pip install -r requirements.txt
+rm tPatchGNN/run_models.py
+cd ..
+
+# 10. Install CUDA libraries (optional but safe)
 conda install --yes cuda -c nvidia
 pip install pandas==1.3.0
 
-# 10. Move custom scripts
+# 11. Move custom scripts
 cp ./custom_scripts/txgnn/test_txgnn.py ./TxGNN/test_txgnn.py
 cp ./custom_scripts/txgnn/utils.py ./TxGNN/utils.py
 cp ./custom_scripts/vqgraph/test_vqgraph.py ./VQGraph/test_vqgraph.py
 cp ./custom_scripts/vqgraph/benchmark_utils.py ./VQGraph/benchmark_utils.py
+cp ./custom_scripts/tPatchGNN/run_models.py ./t-PatchGNN/tPatchGNN/run_models.py
+cp ./custom_scripts/tPatchGNN/perf_utils.py ./t-PatchGNN/tPatchGNN/perf_utils.py
+
+# 12. Run tPatchGNN (not sure if this is where this should go)
+cd t-PatchGNN/tPatchGNN
+python run_models.py --dataset activity --layerwise_csv --gpu 0
 
 sed -i 's/from google_drive_downloader import GoogleDriveDownloader as gdd/import googledrivedownloader as gdd/' VQGraph/dataloader.py
 
